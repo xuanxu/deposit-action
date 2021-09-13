d178c355514fe4e076049f6f42019a9e7d2f0cce
Juanjo Bazán <jjbazan@gmail.com>
2021-09-13 14:34:19 +0200

    add script for paper deposit with journal

---
+++ deposit_with_openjournals.rb
@@ -0,0 +1,21 @@
+require "faraday"
+
+issue_id = ENV["ISSUE_ID"]
+journal_url = "#{ENV['JOURNAL_URL']}/papers/api_deposit",
+journal_secret = ENV['JOURNAL_SECRET']
+
+parameters = { id: issue_id
+               #metadata: Base64.encode64(deposit_payload.to_json),
+               #doi: formatted_doi,
+               #archive_doi: archive_doi,
+               #citation_string: citation_string,
+               #title: plain_title,
+               secret: journal_secret
+              }
+
+deposit_call = Faraday.post(url, parameters.to_json, {})
+if deposit_call.status.between?(200, 299)
+  puts "Deposit looks good."
+else
+  raise "!! ERROR: Something went wrong with this deposit when calling #{journal_url}"
+end
