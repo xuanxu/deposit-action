154c6cf27249a7ba005b2f29874d7c1b91a58e57
Juanjo Bazán <jjbazan@gmail.com>
2022-01-26 11:55:23 +0100

    allow test calls

--- deposit_with_crossref.rb
+++ deposit_with_crossref.rb
@@ -3,10 +3,13 @@ require "faraday"
 crossref_filepath = ENV['CROSSREF_FILEPATH']
 crossref_username = ENV['CROSSREF_USERNAME']
 crossref_password = ENV['CROSSREF_PASSWORD']
+mode = ENV['CROSSREF_MODE']
 
 crossref_url = "https://doi.crossref.org"
 crossref_path = "/servlet/deposit"
 params = { login_id: crossref_username, login_passwd: crossref_password }
+
+crossref_url = "https://test.crossref.org" if mode.to_s.downcase == "test"
 
 if File.exists?(crossref_filepath)
   system("echo 'Depositing with Crossref...'")
@@ -22,7 +25,7 @@ if File.exists?(crossref_filepath)
   if deposit_call.status.between?(200, 299)
     system("echo 'Deposit looks good.'")
   else
-    raise "!! ERROR: Something went wrong with this deposit."
+    raise "!! ERROR: Something went wrong with this deposit call to Crossref"
   end
 else
   raise "!! ERROR: Can't deposit Crossref metadata - deposit XML is missing"
